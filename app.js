ca3b677182c4cfbb9ae0c7bc5814c189cda940f2
Emelly Caixeta <emelleyvitoria2016@gmail.com>
2025-02-04 23:26:01 -0300

    Adiciona tentativas e dicas ao jogo do número secreto

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 alert("Seja bem vindo ao jogo do número secreto!");
-
 let numeroSecreto = 29;
-let chute = prompt("Escolha um número entre 1 e 30");
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto == chute) {
-    console.log("Parabéns! você conseguiu descobrir o número secreto.")
-}
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 30");
+
+    if (chute == numeroSecreto) {
+        break;
+    }else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        }else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }    
+}/*
+if (tentativas > 1) {
+    alert(`Parabéns! você conseguiu descobrir o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Parabéns! você conseguiu descobrir o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+}*/
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! você conseguiu descobrir o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
